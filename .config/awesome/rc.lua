129ef1a9cdd79713bb9621130276e68e0804c0cf
tramhao <haoxinkui@gmail.com>
2019-05-15 10:13:22 +0800

    dropdown pad added

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -305,13 +305,16 @@ globalkeys = my_table.join(
     awful.key({ modkey }, "w", function () awful.util.spawn( browser1 ) end,
         {description = browser1, group = "function keys"}),
     -- dmenu
-    awful.key({ modkey, "Shift"   }, "d",
+    awful.key({ modkey,     }, "d",
     function ()
         awful.spawn(string.format("dmenu_run -i -nb '#191919' -nf '#fea63c' -sb '#fea63c' -sf '#191919' -fn NotoMonoRegular:bold:pixelsize=14",
         beautiful.bg_normal, beautiful.fg_normal, beautiful.bg_focus, beautiful.fg_focus))
 	end,
     {description = "show dmenu", group = "hotkeys"}),
-
+    -- F12 dropdown pad
+    awful.key({}, "F12",function ()
+        awful.util.spawn("tdrop -ma -w 42% -h 75% -x 57% -y 24% st") end,
+	{description = "Dropdown Pad", group = "function keys"}),
 
     -- super + ... function keys
     awful.key({ modkey }, "F1", function () awful.util.spawn( browser1 ) end,
