51433592d9fb5876cbe5ffcacfda049602761b42
tramhao <haoxinkui@gmail.com>
2020-11-24 20:08:55 +0800

    remove autorandr

--- .config/awesome/autostart.sh
+++ .config/awesome/autostart.sh
@@ -50,7 +50,11 @@ run fcitx5
 if [ $(hostname) = "XMLaptop" ]; then
 	run nextcloud
 	run termite
-	run autorandr --change
+	[ -n "$(xrandr | grep 'DP1 connected')" ] && \
+#		echo "hello"
+		xrandr --output DP1 --primary --mode 1920x1200 \
+		--pos 0x0 --rotate normal --output eDP1 --off --output HDMI2 --auto
+	#run autorandr --change
 fi
 run compton -b --config $HOME/.config/awesome/compton.conf
 
