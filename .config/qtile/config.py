f352ed4df45031e4309ead73aa87d92118a6bbf7
tramhao <haoxinkui@gmail.com>
2020-12-12 11:32:48 +0800

    fix qtile widget check updates

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -304,7 +304,7 @@ primary_widgets = [
 
     icon(bg="color4", text=' '), # Icon: nf-fa-download
 
-    widget.CheckUpdates(**base(bg='color4'), no_update_string=' ', update_interval=300,custom_command='yay -Qu', execute=terminal+' --hold -e yay -Syu'),
+    widget.CheckUpdates(**base(bg='color4'), no_update_string=' ', update_interval=300,custom_command='checkupdates', execute=terminal+' --hold -e yay -Syu'),
 
     powerline('color2','color4'),
     icon(bg="color2",text=''),
