8bbff42a6ff945d44ee44040bbaf431b109c85d0
tramhao <haoxinkui@gmail.com>
2020-12-19 22:14:14 +0800

    fix something

--- .config/polybar/scripts/wallpaper.sh
+++ .config/polybar/scripts/wallpaper.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
-wallpaperdir="/home/tramhao/Pictures/wallpapers"
+#!/usr/bin/env bash
+wallpaperdir=$HOME/Pictures/wallpapers
 
 
-files=($wallpaperdir/*.jpg)
-randompic=`printf "%s\n" "${files[RANDOM % ${#files[@]}]}"`
+#files=($wallpaperdir/*.jpg)
+#randompic=`printf "%s\n" "${files[RANDOM % ${#files[@]}]}"`
+
+randompic=$(find $wallpaperdir -type f -iname "*.jpg" -print0 | shuf -z -n 1)
 
 feh --bg-fill $randompic
 #gconftool-2 -t str --set /desktop/gnome/background/picture_filename "$randompic"
