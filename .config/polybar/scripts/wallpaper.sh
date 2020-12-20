393a9ff0a061bff7bfc81afe6f3e678ea2a57160
tramhao <haoxinkui@gmail.com>
2020-12-20 22:39:47 +0800

    wallpaper widget is still not working all right. click is not working

--- .config/polybar/scripts/wallpaper.sh
+++ .config/polybar/scripts/wallpaper.sh
@@ -5,7 +5,9 @@ wallpaperdir=$HOME/Pictures/wallpapers
 #files=($wallpaperdir/*.jpg)
 #randompic=`printf "%s\n" "${files[RANDOM % ${#files[@]}]}"`
 
-randompic=$(find $wallpaperdir -type f -iname "*.jpg" -print0 | shuf -z -n 1)
+randompic=$(find $wallpaperdir -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" \) -print0 | shuf -z -n 1 | tr '\0' '\n')
+#randompic=$(find $wallpaperdir -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" \) -print0 | shuf -z -n 1)
 
-feh --bg-fill $randompic
+feh --bg-fill "$randompic"
+echo "  "
 #gconftool-2 -t str --set /desktop/gnome/background/picture_filename "$randompic"
