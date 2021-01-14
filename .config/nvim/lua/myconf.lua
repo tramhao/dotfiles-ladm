f82fd203b0a195b6de01a84dd031a90aeb0bf57a
tramhao <haoxinkui@gmail.com>
2021-01-14 16:24:11 +0800

    sldkjf

--- .config/nvim/lua/myconf.lua
+++ .config/nvim/lua/myconf.lua
@@ -3,7 +3,7 @@ local cmd = vim.cmd  -- to execute Vim commands e.g. cmd('pwd')
 local g = vim.g      -- a table to access global variables
 local scopes = {o = vim.o, b = vim.bo, w = vim.wo}
 local indent = 2
-
+-- local opti = vim.opt
 local function opt(scope, key, value)
   scopes[scope][key] = value
   if scope ~= 'o' then scopes['o'][key] = value end
