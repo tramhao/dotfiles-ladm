1ef3fa5ca8f9f5e479a530ba90da7930a3d383d6
tramhao <haoxinkui@gmail.com>
2021-03-19 10:36:33 +0800

    sdf

--- .config/nvim/lua/configplugin/lsp.lua
+++ .config/nvim/lua/configplugin/lsp.lua
@@ -3,7 +3,6 @@ return function()
 -- set the path to the sumneko installation; if you previously installed via the now deprecated :LspInstall, use
 local sumneko_root_path = '/usr/share/lua-language-server'
 local sumneko_binary = "/usr/bin/lua-language-server"
--- local compe = require('compe')
 -- local completion = require('completion')
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true;
@@ -19,22 +18,9 @@ local custom_init = function (client)
 end
 local custom_attach = function()
 -- local custom_attach = function(client, bufnr)
-    -- compe.on_attach(client, bufnr)
     -- completion.on_attach(client, bufnr)
-    -- Keybindings for LSPs
-    -- vim.fn.nvim_set_keymap("n", "<leader>gd", "<cmd>lua vim.lsp.buf.definition()<CR>", {noremap = true, silent = true})
-    -- vim.fn.nvim_set_keymap("n", "<leader>ge", "<cmd>lua vim.lsp.buf.declaration()<CR>", {noremap = true, silent = true})
-    -- vim.fn.nvim_set_keymap("n", "<leader>gh", "<cmd>lua vim.lsp.buf.hover()<CR>", {noremap = true, silent = true})
-    -- vim.fn.nvim_set_keymap("n", "<leader>gf", "<cmd>lua vim.lsp.buf.formatting()<CR>", {noremap = true, silent = true})
-    -- vim.fn.nvim_set_keymap("n", "<leader>gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", {noremap = true, silent = true})
-    -- vim.fn.nvim_set_keymap("n", "<c-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", {noremap = true, silent = true})
-    -- vim.fn.nvim_set_keymap("n", "<leader>gy", "<cmd>lua vim.lsp.buf.type_definition()<CR>", {noremap = true, silent = true})
-    -- vim.fn.nvim_set_keymap("n", "<leader>gr", "<cmd>lua vim.lsp.buf.references()<CR>", {noremap = true, silent = true})
-    -- vim.fn.nvim_set_keymap("n", "<leader>gt", "<cmd>lua vim.lsp.buf.document_symbol()<CR>", {noremap = true, silent = true})
-    -- vim.fn.nvim_set_keymap("n", "<leader>gw", "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>", {noremap = true, silent = true})
-    -- vim.fn.nvim_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", {noremap = true, silent = true})
-    -- vim.fn.nvim_set_keymap("n", "<a-.>", "<cmd>lua vim.lsp.buf.code_action()<CR>", {noremap = true, silent = true})
     -- vim.api.nvim_command('setlocal omnifunc=v:lua.vim.lsp.omnifunc')
+    -- Keybindings for LSPs
     print("LSP started.");
     map('n','gD','<cmd>lua vim.lsp.buf.declaration()<CR>')
     map('n','gd','<cmd>lua vim.lsp.buf.definition()<CR>')
@@ -54,7 +40,7 @@ local custom_attach = function()
     map('n','<leader>ao','<cmd>lua vim.lsp.buf.outgoing_calls()<CR>')
     map('n','<C-n>','<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
     map('n','<C-p>','<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
-  end
+end
    nvim_lsp.bashls.setup{
        cmd = {"/usr/bin/bash-language-server", "start"},
        filetype = {"sh"},
@@ -294,4 +280,5 @@ local custom_attach = function()
   nvim_lsp.angularls.setup{
     on_attach = custom_attach
   }
+
 end
