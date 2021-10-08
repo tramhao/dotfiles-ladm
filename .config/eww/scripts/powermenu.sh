e6ae773642e27cb5be8d81802fb65dc6186f8ed2
tramhao <haoxinkui@gmail.com>
2021-10-09 01:20:59 +0800

    working

--- .config/eww/scripts/powermenu.sh
+++ .config/eww/scripts/powermenu.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
+rofi -show p -modi p:rofi-power-menu \
+  -theme Paper \
+  -font "JetBrains Mono NF 30" \
+  -width 20 \
+  -lines 6
+# chosen=$(echo -e "[Cancel]\nLogout\nShutdown\nReboot\nSuspend\nLock\n" | rofi -dmenu -i -font "hack 30")
+# # chosen=$(echo -e "[Cancel]\nLogout\nShutdown\nReboot\nSuspend\nHibernate\nHybrid-sleep\nSuspend-then-hibernate" | rofi -dmenu -i)
 
-chosen=$(echo -e "[Cancel]\nLogout\nShutdown\nReboot\nSuspend\nLock\n" | rofi -dmenu -i -font "hack 30")
-# chosen=$(echo -e "[Cancel]\nLogout\nShutdown\nReboot\nSuspend\nHibernate\nHybrid-sleep\nSuspend-then-hibernate" | rofi -dmenu -i)
-
-if [[ $chosen = "Logout" ]]; then
-    loginctl kill-session $XDG_SESSION_ID 
- # pkill leftwm
-elif [[ $chosen = "Shutdown" ]]; then
-	systemctl poweroff
-elif [[ $chosen = "Reboot" ]]; then
-	systemctl reboot
-elif [[ $chosen = "Suspend" ]]; then
-	systemctl suspend
-elif [[ $chosen = "Lock" ]]; then
-    slock
-elif [[ $chosen = "Hybrid-sleep" ]]; then
-	systemctl hibernate
-elif [[ $chosen = "Suspend-then-hibernate" ]]; then
-	systemctl suspend-then-hibernate
-fi
+# if [[ $chosen = "Logout" ]]; then
+#     exec loginctl kill-session $XDG_SESSION_ID 
+# # pkill leftwm
+# elif [[ $chosen = "Shutdown" ]]; then
+# 	exec systemctl poweroff
+# elif [[ $chosen = "Reboot" ]]; then
+# 	exec systemctl reboot
+# elif [[ $chosen = "Suspend" ]]; then
+# 	exec systemctl suspend
+# elif [[ $chosen = "Lock" ]]; then
+#     exec slock
+# elif [[ $chosen = "Hybrid-sleep" ]]; then
+# 	exec systemctl hibernate
+# elif [[ $chosen = "Suspend-then-hibernate" ]]; then
+# 	exec systemctl suspend-then-hibernate
+# fi
