a9f639e66d5d6c156f7b7603ba6a7252a2e4d6fd
tramhao <haoxinkui@gmail.com>
2021-10-24 15:12:15 +0800

    cmp update

--- .config/nvim/lua/configplugin/cmp.lua
+++ .config/nvim/lua/configplugin/cmp.lua
@@ -1,4 +1,8 @@
 return function()
+	-- Set completeopt to have a better completion experience
+	vim.o.completeopt = "menu,menuone,noselect"
+	--" Avoid showing message extra message when using completion
+	vim.api.nvim_command("set shortmess+=c")
 	-- INIT
 	local cmp = require("cmp")
 	local luasnip = require("luasnip")
@@ -36,33 +40,6 @@ return function()
 					fallback()
 				end
 			end,
-			-- ["<Tab>"] = cmp.mapping(function(fallback)
-			-- 	if vim.fn.pumvisible() == 1 then
-			-- 		vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-n>", true, true, true), "n")
-			-- 	elseif luasnip.expand_or_jumpable() then
-			-- 		vim.fn.feedkeys(
-			-- 			vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true),
-			-- 			""
-			-- 		)
-			-- 	else
-			-- 		fallback()
-			-- 	end
-			-- end, {
-			-- 	"i",
-			-- 	"s",
-			-- }),
-			-- ["<S-Tab>"] = cmp.mapping(function(fallback)
-			-- 	if vim.fn.pumvisible() == 1 then
-			-- 		vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-p>", true, true, true), "n")
-			-- 	elseif luasnip.jumpable(-1) then
-			-- 		vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
-			-- 	else
-			-- 		fallback()
-			-- 	end
-			-- end, {
-			-- 	"i",
-			-- 	"s",
-			-- }),
 		},
 		sources = {
 			{ name = "nvim_lsp" },
@@ -76,77 +53,44 @@ return function()
 			{ name = "treesitter" },
 			{ name = "crates" },
 		},
-		formatting = {
-			format = function(entry, vim_item)
-				local icons = require("lsp.kind").icons
-				vim_item.kind = icons[vim_item.kind]
-				vim_item.menu = ({
-					nvim_lsp = "(LSP)",
-					emoji = "(Emoji)",
-					path = "(Path)",
-					calc = "(Calc)",
-					cmp_tabnine = "(Tabnine)",
-					vsnip = "(Snippet)",
-					luasnip = "(Snippet)",
-					buffer = "(Buffer)",
-				})[entry.source.name]
-				vim_item.dup = ({
-					buffer = 1,
-					path = 1,
-					nvim_lsp = 0,
-				})[entry.source.name] or 0
-				return vim_item
-			end,
-		},
+		-- formatting = {
+		-- 	format = function(entry, vim_item)
+		-- 		local icons = require("lsp.kind").icons
+		-- 		vim_item.kind = icons[vim_item.kind]
+		-- 		vim_item.menu = ({
+		-- 			nvim_lsp = "(LSP)",
+		-- 			emoji = "(Emoji)",
+		-- 			path = "(Path)",
+		-- 			calc = "(Calc)",
+		-- 			cmp_tabnine = "(Tabnine)",
+		-- 			vsnip = "(Snippet)",
+		-- 			luasnip = "(Snippet)",
+		-- 			buffer = "(Buffer)",
+		-- 		})[entry.source.name]
+		-- 		vim_item.dup = ({
+		-- 			buffer = 1,
+		-- 			path = 1,
+		-- 			nvim_lsp = 0,
+		-- 		})[entry.source.name] or 0
+		-- 		return vim_item
+		-- 	end,
+		-- },
+        formatting = {
+          format = require("lspkind").cmp_format({with_text = true,maxwidth = 50, menu = ({
+              buffer = "(Buffer)",
+              nvim_lsp = "(LSP)",
+              luasnip = "(LuaSnip)",
+              nvim_lua = "(Lua)",
+              latex_symbols = "(Latex)",
+              emoji = "(Emoji)",
+              path = "(Path)",
+              calc = "(Calc)",
+              cmp_tabnine= "(Tabnine)",
+              vsnip = "(Snippet)",
+            })}),
+        },
 		documentation = {
 			border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
 		},
 	})
-
-	-- SET SNIPPETS PATH
-	-- vim.g.vsnip_snippet_dir = vim.fn.stdpath("config") .. "/snippets"
-	-- vim.g.vsnip_filetypes = {
-	-- 	javascriptreact = { "javascript", "html" },
-	-- 	typescriptreact = { "typescript", "html" },
-	-- }
-
-	-- Set completeopt to have a better completion experience
-	vim.o.completeopt = "menu,menuone,noselect"
-	-- api.nvim_command('set completeopt+=menu,longest,menuone,noinsert,noselect') --> This cuases small error with compe, might be noinsert
-
-	-- The above mapping will change the behavior of the <Enter> key when the popup menu is visible. In that case the Enter key will simply select the highlighted menu item, just as <C-Y> does. Does not seem to be neccessary right now with my config but leaving as inspo
-	--api.nvim_command('inoremap <expr> <CR> pumvisible() ? "\\<C-y>" : "\\<C-g>u\\<CR>"')
-
-	--" Avoid showing message extra message when using completion
-	vim.api.nvim_command("set shortmess+=c")
-
-	-- CONFIGURES ICONS NEXT TO COMPLETION ITEMS FOR LSP
-	-- HOW CAN WE ADD BUFFER ICONS?
-	vim.lsp.protocol.CompletionItemKind = {
-		" Text", -- = 1
-		"ƒ Method", -- = 2;
-		" Function", -- = 3;
-		" Constructor", -- = 4;
-		"ƒ Field", -- = 5;
-		" Variable", -- = 6;
-		" Class", -- = 7;
-		"ﰮ Interface", -- = 8;
-		" Module", -- = 9;
-		" Property", -- = 10;
-		" Unit", -- = 11;
-		" Value", -- = 12;
-		"了Enum", -- = 13;
-		" Keyword", -- = 14;
-		"﬌ Snippet", -- = 15;
-		" Color", -- = 16;
-		" File", -- = 17;
-		"Reference", -- = 18;
-		" Folder", -- = 19;
-		" EnumMember", -- = 20;
-		" Constant", -- = 21;
-		" Struct", -- = 22;
-		"Event", -- = 23;
-		"Operator", -- = 24;
-		"TypeParameter", -- = 25;
-	}
 end
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -72,6 +72,7 @@ return require("packer").startup(function()
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-path",
 			"hrsh7th/cmp-nvim-lua",
+            "onsails/lspkind-nvim"
 		},
 		config = require("configplugin.cmp"),
 	})
