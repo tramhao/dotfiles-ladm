9f0fa3b4eac720a4da13b8c89768077e1d37cbf1
tramhao <haoxinkui@gmail.com>
2023-02-07 10:46:05 +0800

    abc

--- .config/hypr/configure_2nd_monitor.sh
+++ .config/hypr/configure_2nd_monitor.sh
@@ -3,16 +3,27 @@
 if [ $(uname -n) = "lenovo-t480" ]; then
   if [ `ifconfig wlan0 | awk '/inet /{ print $6;}'` = "192.168.186.255" ]; then
       sleep 2
-      hyprctl --batch "keyword monitor HDMI-A-2, preferred,0x0,1; \ 
-      keyword monitor eDP-1, preferred,1920x0,1; \
-      keyword monitor eDP-1, transform,3; \
-      keyword wsbind 1, HDMI-A-2; \
-      keyword wsbind 2, HDMI-A-2; \
-      keyword wsbind 3, HDMI-A-2; \
-      keyword wsbind 4, HDMI-A-2; \
-      keyword wsbind 5, HDMI-A-2; \
-      keyword wsbind 6, HDMI-A-2; \
-      keyword wsbind 7, eDP-1"
+      # command="keyword monitor HDMI-A-2, preferred,0x0,1;\  
+      # keyword wsbind 1, HDMI-A-2;\
+      # keyword wsbind 2, HDMI-A-2;\
+      # keyword wsbind 3, HDMI-A-2;\
+      # keyword wsbind 4, HDMI-A-2;\
+      # keyword wsbind 5, HDMI-A-2;\
+      # keyword wsbind 6, HDMI-A-2;\
+      # keyword monitor eDP-1, preferred,1920x0,1;\
+      # keyword monitor eDP-1, transform,3;\
+      # keyword wsbind 7, eDP-1"
+      # hyprctl --batch "$command"
+      hyprctl keyword monitor HDMI-A-2, preferred,0x0,1  
+      hyprctl keyword wsbind 1, HDMI-A-2
+      hyprctl keyword wsbind 2, HDMI-A-2
+      hyprctl keyword wsbind 3, HDMI-A-2
+      hyprctl keyword wsbind 4, HDMI-A-2
+      hyprctl keyword wsbind 5, HDMI-A-2
+      hyprctl keyword wsbind 6, HDMI-A-2
+      hyprctl keyword monitor eDP-1, preferred,1920x0,1
+      hyprctl keyword monitor eDP-1, transform,3
+      hyprctl keyword wsbind 7, eDP-1
   fi
 fi
 
