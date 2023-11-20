7957a4972e991abe23470049dfabb7db38c9dfed
tramhao <haoxinkui@gmail.com>
2023-11-20 11:28:00 +0800

    monitor switch for hyprland

---
+++ .config/hypr/switch.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if grep open /proc/acpi/button/lid/LID0/state; then
+    hyprctl keyword monitor "eDP-1, preferred, 0x0, 1.6"
+else
+    if [[ `hyprctl monitors | grep "Monitor" | wc -l` != 1 ]]; then
+        hyprctl keyword monitor "eDP-1, disable"
+    fi
+fi
